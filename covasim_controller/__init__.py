a1b9e9a4883354838e0178a04f6514423c35ff01
Daniel J. Klein <dklein@idmod.org>
2021-01-26 14:42:08 -0800

    A bit of cleaning, and improvements to transition matrix fitting

--- covasim_controller/__init__.py
+++ covasim_controller/__init__.py
@@ -1,8 +1,7 @@
 from .version import __version__, __versiondate__
-from .school_pop import *
+#from .school_pop import *
 from .controller_intervention import *
 from .controller import *
 from .SEIR import *
 from .TransitionMatrix import *
 from .Kalman import *
-from .ReferenceTrajectory import *
