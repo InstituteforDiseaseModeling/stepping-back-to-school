ac667f1c25cdbc7f6fcec1e742f16cb05f22e6e0
cliffckerr <questionably@gmail.com>
2021-02-03 21:44:35 -0800

    first two updated

--- scripts/outbreak_betasch.py
+++ scripts/outbreak_betasch.py
@@ -8,17 +8,17 @@ import school_tools as sct
 
 if __name__ == '__main__':
 
-    args = cfg.process_inputs(sys.argv)
+    # Settings
+
+    args = sct.config.process_inputs(sys.argv)
 
     sweep_pars = {
-        'n_prev':       0, # No controller
+        'n_prev': 0, # No controller
         'school_start_date': '2021-02-01',
         'school_seed_date': '2021-02-01',
-        #'screen_keys':  ['None'],
-        #'schcfg_keys':  ['with_countermeasures'],
     }
 
-    pop_size = cfg.sim_pars.pop_size
+    pop_size = sct.config.sim_pars.pop_size
     sim_pars = {
         'pop_infected': 0, # Do not seed
         'pop_size': pop_size,
@@ -30,25 +30,22 @@ if __name__ == '__main__':
     npi_scens = {x:{'beta_s': 1.5*x} for x in np.linspace(0, 2, 10)}
     levels = [{'keyname':'In-school transmission multiplier', 'level':npi_scens, 'func':'screenpars_func'}]
 
-    runner = OutbreakBetaSchool(sweep_pars=sweep_pars, sim_pars=sim_pars, levels=levels)
-    runner.run(args.force)
-    analyzer = runner.analyze()
+    xvar = 'In-school transmission multiplier'
+    huevar = None
 
-    analyzer.outbreak_size_distribution(row='In-school transmission multiplier', col=None)
+    # Create and run
+    mgr = sct.Manager(sweep_pars=sweep_pars, sim_pars=sim_pars, levels=levels)
+    mgr.run(args.force)
+    analyzer = mgr.analyze()
 
+    # Plots
+    analyzer.outbreak_size_distribution(row=xvar, col=None)
     analyzer.outbreak_R0()
-
-    xvar='In-school transmission multiplier'
-    huevar=None
-
-    #runner.regplots(xvar=xvar, huevar=huevar)
     analyzer.exports_reg(xvar, huevar)
     analyzer.outbreak_reg(xvar, huevar)
     analyzer.outbreak_size_plot(xvar, scatter=True, loess=False)
     analyzer.outbreak_size_plot(xvar, scatter=False, loess=True) # Included for completeness but looks weird
-
     analyzer.cum_incidence(colvar=xvar)
     analyzer.outbreak_size_over_time()
     analyzer.source_pie()
-
-    runner.tsplots()
+    mgr.tsplots()
