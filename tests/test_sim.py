63cda08e0e3003a3ef03409f2aa1e77921b3a11b
cliffckerr <questionably@gmail.com>
2021-02-03 19:11:26 -0800

    slightly smaller

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -3,12 +3,12 @@ Very simple test of sim creation -- not part of the main library
 '''
 
 import covasim as cv
-from risk_evaluation import create_sim as cs
+import school_tools as sct
 
 def test_sims(do_plot=False):
-    s1 = cs.create_sim(pop_size=5e3, load_pop=False, label='Default')
-    s2 = cs.create_sim(pop_size=5e3, load_pop=False, rand_seed=23948, label='Different seed')
-    s3 = cs.create_sim(pop_size=5e3, load_pop=False, beta=0.03, label='High beta')
+    s1 = sct.create_sim(pop_size=3e3, load_pop=False, label='Default')
+    s2 = sct.create_sim(pop_size=3e3, load_pop=False, rand_seed=23948, label='Different seed')
+    s3 = sct.create_sim(pop_size=3e3, load_pop=False, beta=0.03, label='High beta')
 
     msim = cv.MultiSim([s1, s2, s3])
     msim.run()
