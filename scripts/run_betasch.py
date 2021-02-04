bf5a7128bdaa4561fc5579db9866b7672803aa30
cliffckerr <questionably@gmail.com>
2021-02-03 22:16:04 -0800

    first run script

--- scripts/run_betasch.py
+++ scripts/run_betasch.py
@@ -2,38 +2,35 @@
 Dense sweep of in-school transmissibility (beta_s) at a few fixed prevalence levels.
 '''
 
-import argparse
-from run import Run
+import sys
 import numpy as np
-
-class BetaSchool(Run):
-    def build_configs(self):
-        # Sweep over NPI multipliers
-        npi_scens = {x:{'beta_s': 1.5*x} for x in np.linspace(0, 2, 10)}
-        self.builder.add_level('In-school transmission multiplier', npi_scens, self.builder.screenpars_func)
-
-        return super().build_configs()
-
+import school_tools as sct
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--force', action='store_true')
-    args = parser.parse_args()
+
+    # Settings
+
+    args = sct.config.process_inputs(sys.argv)
 
     sim_pars = dict(pop_size=223_000, end_day='2021-04-30')
+    sweep_pars = dict(n_reps=10, prev=[0.002, 0.007, 0.014])
 
-    runner = BetaSchool(sweep_pars=dict(n_reps=10, prev=[0.002, 0.007, 0.014]), sim_pars=sim_pars)
-    runner.run(args.force)
-    analyzer = runner.analyze()
-    #runner.plot(xvar='In-school transmission multiplier', huevar='Prevalence', ts_plots=True, order=3)
+    npi_scens = {x:{'beta_s': 1.5*x} for x in np.linspace(0, 2, 10)}
+    levels = [{'keyname':'In-school transmission multiplier', 'level':npi_scens, 'func':'screenpars_func'}]
 
-    runner.regplots(xvar='In-school transmission multiplier', huevar=None)
+    xvar = 'In-school transmission multiplier'
+    huevar = None
 
-    analyzer.outbreak_reg(xvar='In-school transmission multiplier', huevar=None, height=5, aspect=2, ext='_wide')
+    # Create and run
+    mgr = sct.Manager(sweep_pars=sweep_pars, sim_pars=sim_pars, levels=levels)
+    mgr.run(args.force)
+    analyzer = mgr.analyze()
 
-    analyzer.cum_incidence(colvar='In-school transmission multiplier')
-    analyzer.introductions_rate_by_stype(xvar='In-school transmission multiplier')
+    # Plots
+    mgr.regplots(xvar=xvar, huevar=huevar)
+    analyzer.outbreak_reg(xvar=xvar, huevar=huevar, height=5, aspect=2, ext='_wide')
+    analyzer.cum_incidence(colvar=xvar)
+    analyzer.introductions_rate_by_stype(xvar=xvar)
     analyzer.outbreak_size_over_time()
     analyzer.source_pie()
-
-    runner.tsplots()
+    mgr.tsplots()
