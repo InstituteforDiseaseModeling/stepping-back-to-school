14b686e2db43c98b4bf519bbcff153b4b4c77a5b
cliffckerr <questionably@gmail.com>
2021-02-04 16:11:58 -0800

    attempt 3

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "seaborn",
         "sciris>=1.0.0",
         "covasim>=2.0.0",
-        "synthpops @ git+https://github.com/institutefordiseasemodeling/synthpops@master",
+        "-e git+https://github.com/institutefordiseasemodeling/synthpops.git",
         "sklearn",
     ],
 )
