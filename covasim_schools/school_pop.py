f03b59d771e582ca8e7657ad88eddcc4666a0d8b
cliffckerr <questionably@gmail.com>
2021-02-09 10:39:11 -0800

    move plotting method to class

--- covasim_schools/school_pop.py
+++ covasim_schools/school_pop.py
@@ -5,10 +5,71 @@ Generate a SynthPops population for use with the schools code.
 import os
 import numpy as np
 import sciris as sc
+import pylab as pl
 import covasim as cv
 import synthpops as sp
 
+
+
 n_brackets = 20 # This is required...maybe
+
+
+class SchoolPeople(cv.People):
+    ''' Subclass of People to add an additional plotting method '''
+
+    def plot_schools(self, figsize=None, dpi=None, to_json=False, outfile=None):
+        ''' Sanity check for school distributions '''
+
+        # Custom configuration
+        keys = ['pk', 'es', 'ms', 'hs'] # Exclude universities for this analysis
+        ppl_keys = ['all', 'students', 'teachers', 'staff']
+        xpeople = np.arange(len(ppl_keys)) # X axis for people
+        school_types_by_ind = {}
+        for key,vals in self.school_types.items():
+            for val in vals:
+                if key in keys:
+                    school_types_by_ind[val] = key
+
+        results = {}
+        for sc_id,sc_type in school_types_by_ind.items():
+            thisres = sc.objdict()
+            sc_inds = (self.school_id == sc_id)
+            thisres.all = cv.true(sc_inds)
+            thisres.students = cv.true(np.array(self.student_flag) * sc_inds)
+            thisres.teachers = cv.true(np.array(self.teacher_flag) * sc_inds)
+            thisres.staff    = cv.true(np.array(self.staff_flag) * sc_inds)
+            results[sc_id] = thisres
+
+        # Do plotting
+        pl.figure(figsize=figsize, dpi=dpi)
+        pl.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95, hspace=0.5, wspace=0.5)
+        n_schools = len(results)
+        n_cols = len(ppl_keys) + 1
+        count = 0
+        for sc_id in results.keys():
+            count += 1
+            school_type = school_types_by_ind[sc_id]
+            ax = pl.subplot(n_schools, n_cols, count)
+            thisres = results[sc_id]
+            thisres.people_counts = [len(thisres[k]) for k in ppl_keys]
+            ax.bar(xpeople, thisres.people_counts)
+            ax.set_xticks(xpeople)
+            ax.set_xticklabels(ppl_keys)
+            title = f'School ID {sc_id}, school type {school_type}, total size: {len(thisres.all)}'
+            ax.set_title(title)
+
+            thisres.ages = sc.objdict()
+            for key in ppl_keys:
+                count += 1
+                ax = pl.subplot(n_schools, n_cols, count)
+                thisres.ages[key] = self.age[thisres[key]]
+                pl.hist(thisres.ages[key])
+                ax.set_title(f'Ages for {key} in school {sc_id} ({school_type})')
+
+        if to_json:
+            sc.savejson(outfile, results, indent=2)
+
+        return results
 
 
 def pop_path(popfile=None, location=None, folder=None, strategy=None, n=None, rand_seed=None):
@@ -138,7 +199,8 @@ def make_population(pop_size, rand_seed=1, max_pop_seeds=None, do_save=True, loc
         beta = 1.0, # TODO: this is required for plotting (people.plot()), but shouldn't be
     )
 
-    people = cv.People(people_pars, strict=False, uid=popdict['uid'], age=popdict['age'], sex=popdict['sex'],
+    # Identical to a Covasim People instance except with the plotting method defined above
+    people = SchoolPeople(people_pars, strict=False, uid=popdict['uid'], age=popdict['age'], sex=popdict['sex'],
                           contacts=popdict['contacts'], school_id=np.array(school_ids),
                           schools=schools, school_types=school_types,
                           student_flag=student_flag, teacher_flag=teacher_flag,
