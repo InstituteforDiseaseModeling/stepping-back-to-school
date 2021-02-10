28b5417ed43d2a631903c990e368d491f46c6d3d
cliffckerr <questionably@gmail.com>
2021-02-09 19:00:10 -0800

    add equality test

--- tests/test_pop.py
+++ tests/test_pop.py
@@ -2,6 +2,7 @@
 Very simple test of population generation
 '''
 
+import sciris as sc
 import covasim_schools as cvsch
 
 
@@ -17,6 +18,24 @@ def test_school_pop(pop_size=5e3, do_plot=False):
     return pop
 
 
+def test_pop_equality(pop_size=5e3):
+    pops = sc.objdict()
+    pops.seed1a = cvsch.make_population(pop_size=pop_size, rand_seed=1, do_save=False)
+    pops.seed1b = cvsch.make_population(pop_size=pop_size, rand_seed=1, do_save=False)
+    pops.seed2a = cvsch.make_population(pop_size=pop_size, rand_seed=2, do_save=False)
+    pops.seed2b = cvsch.make_population(pop_size=pop_size, rand_seed=2, do_save=False)
+
+    n = sc.objdict()
+    for k,pop in pops.items():
+        n[k] = len(pop.contacts)
+        print(f'Population {k} has {n[k]} contacts')
+
+    assert (n.seed1a == n.seed1b) and (n.seed2a == n.seed2b) and (n.seed1a != n.seed2a)
+
+    return
+
+
 if __name__ == '__main__':
     pop = test_school_pop(pop_size=20e3, do_plot=True)
+    test_pop_equality()
 
