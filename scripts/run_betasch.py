2785470dedfcadb0b2174dd5f068304c83843af7
Daniel J. Klein <dklein@idmod.org>
2021-02-18 16:50:56 -0800

    Adding susceptibility sensitity for 1) baseline, 2) equal, 3) viner.

--- scripts/run_betasch.py
+++ scripts/run_betasch.py
@@ -61,14 +61,14 @@ if __name__ == '__main__':
         mgr.tsplots()
 
     else:
-        g = analyzer.outbreak_size_stacked_distrib(xvar, rowvar=None, ext=None, height=6, aspect=2)
+        g = analyzer.outbreak_size_distrib(xvar, rowvar=None, height=6, aspect=1.4, ext='ppt')
 
         # Plots
         g = analyzer.outbreak_multipanel(xvar, ext=None, jitter=0.2, values=None, legend=False, height=12, aspect=1.0) # height=10, aspect=0.7,
 
         analyzer.exports_reg(xvar, huevar)
         analyzer.outbreak_reg(xvar, ext='ppt')
-        analyzer.outbreak_reg(xvar, ext='ppt', by_stype=True)
+        analyzer.outbreak_reg(xvar, ext='stype_ppt', by_stype=True)
         #analyzer.outbreak_reg_by_stype(xvar, height=6, aspect=1.4, ext='ppt', nboot=50, legend=True)
         #analyzer.outbreak_size_plot(xvar) #xvar, rowvar=None, ext=None, height=6, aspect=1.4, scatter=True, jitter=0.012
         analyzer.cum_incidence(colvar=xvar)
@@ -94,7 +94,7 @@ if __name__ == '__main__':
 
             # Panel 1
             ax = axv[0]
-            analyzer.outbreak_size_stacked_distrib(xvar, ax=ax)
+            analyzer.outbreak_size_distrib(xvar, ax=ax)
             ax.set_xticks(xt)
             ax.set_xticklabels([])
             ax.set_xlabel('')
